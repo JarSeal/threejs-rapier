14d57cd63d6c9fba649b75a337f1a86807dc009c
JarSeal <kai.forsman@gmail.com>
2025-01-16 22:15:02 +0100

    SkyBox equirectangular work

--- src/core/SkyBox.ts
+++ src/core/SkyBox.ts
@@ -3,6 +3,8 @@ import { texture, equirectUV, ShaderNodeObject } from 'three/tsl';
 import { lerror, lwarn } from '../utils/Logger';
 import { getCurrentScene, getScene } from './Scene';
 import { getRenderer } from './Renderer';
+import { getTexture, loadTexture } from './Texture';
+import { isDebugEnvironment } from './Config';
 
 type SkyBoxProps = {
   sceneId?: string;
@@ -10,7 +12,8 @@ type SkyBoxProps = {
   | {
       type: 'EQUIRECTANGULAR';
       params: {
-        file: string | THREE.Texture;
+        file?: string | THREE.Texture;
+        textureId?: string;
         colorSpace?: string;
       };
     }
@@ -43,26 +46,43 @@ export const addSkyBox = ({ sceneId, type, params }: SkyBoxProps) => {
 
   if (type === 'EQUIRECTANGULAR') {
     const file = params.file;
-    if (typeof file === 'string') {
-      new THREE.TextureLoader().load(
-        file,
-        (equirectTexture) => {
-          equirectTexture.colorSpace = THREE.SRGBColorSpace;
-          const shaderNodeTexture = texture(equirectTexture, equirectUV(), 0);
-          equirectTexture.dispose();
-          scene.backgroundNode = shaderNodeTexture as unknown as ShaderNodeObject<THREE.Node>;
-        },
-        undefined,
-        (err) => {
-          if (err) {
-            const msg = `Error while loading equirectangular texture for ${sceneId ? `scene with id "${sceneId}"` : 'current scene'} in addSkyBox (type: ${type}, file: ${params.file}).`;
-            lerror(msg, err);
-            throw new Error(msg);
-          }
-        }
-      );
-    } else {
-      file.colorSpace = THREE.SRGBColorSpace;
+    const textureId = params.textureId;
+    if (!file && !textureId) {
+      lerror('Provide either file or textureId in the equirectangular params in addSkyBox.');
+      return;
+    }
+    if (typeof file === 'string' || textureId) {
+      const equirectTexture = file
+        ? loadTexture({
+            id: textureId,
+            fileName: file as string,
+            throwOnError: isDebugEnvironment(),
+          })
+        : getTexture(textureId || '');
+      equirectTexture.colorSpace = params.colorSpace || THREE.SRGBColorSpace;
+      const shaderNodeTexture = texture(equirectTexture, equirectUV(), 0);
+      scene.backgroundNode = shaderNodeTexture as unknown as ShaderNodeObject<THREE.Node>;
+      // @TODO: remove backgroundNode texture in deleteScene
+
+      // new THREE.TextureLoader().load(
+      //   file,
+      //   (equirectTexture) => {
+      //     equirectTexture.colorSpace = THREE.SRGBColorSpace;
+      //     const shaderNodeTexture = texture(equirectTexture, equirectUV(), 0);
+      //     equirectTexture.dispose();
+      //     scene.backgroundNode = shaderNodeTexture as unknown as ShaderNodeObject<THREE.Node>;
+      //   },
+      //   undefined,
+      //   (err) => {
+      //     if (err) {
+      //       const msg = `Error while loading equirectangular texture for ${sceneId ? `scene with id "${sceneId}"` : 'current scene'} in addSkyBox (type: ${type}, file: ${params.file}).`;
+      //       lerror(msg, err);
+      //       throw new Error(msg);
+      //     }
+      //   }
+      // );
+    } else if (file) {
+      file.colorSpace = params.colorSpace || THREE.SRGBColorSpace;
       const shaderNodeTexture = texture(file, equirectUV(), 0);
       scene.backgroundNode = shaderNodeTexture as unknown as ShaderNodeObject<THREE.Node>;
     }
