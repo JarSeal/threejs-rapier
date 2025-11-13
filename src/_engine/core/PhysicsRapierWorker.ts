ca6f220333215da73257c3eeca836cc3778d75d4
JarSeal <kai.forsman@gmail.com>
2025-11-13 20:57:46 +0200

    PhysicsWorker TODO comments

--- src/_engine/core/PhysicsRapierWorker.ts
+++ src/_engine/core/PhysicsRapierWorker.ts
@@ -31,3 +31,8 @@ import {
   deleteAllScenePhysicsLoopers,
   getCurrentScenePhysParams,
 } from './PhysicsRapier';
+
+// @TODO: move this file to the worker folder (create it first)
+// @TODO: refactor the current PhysicsRapier.ts to not return any actual Rapier physics entities (RigidBody, Collider, World etc.).
+// Replace the current physics entities as APIs: eg. physObj = { rigidBody: setLinvel = (Vec3, wakeUp) => { // Custom function here that checks whether we are using threaded version or not and also if we are in the worker or in the main thread. } }
+// Also eg. getPhysicsWorld = () => { // Same check here... }
